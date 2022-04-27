52435af41e4571863741f641ceafb8f10f6ff00b
Silas Waller <97419274+silaswaller@users.noreply.github.com>
2022-04-27 17:40:12 -0500

    Add files via upload

--- Ninja.js
+++ Ninja.js
@@ -7,12 +7,15 @@ class Ninja {
     }
     sayName(){
         console.log(`This ninja's name is ${this.name}`)
+        return this;
     }
     showStats(){
         console.log(`${this.name}'s health is ${this.health}, speed is ${this.speed}, and strength is ${this.strength}`)
+        return this;
     }
     drinkSake(){
         this.health += 10
+        return this;
     }
     
 }
@@ -21,3 +24,18 @@ const ninja1 = new Ninja("Silas", 100)
 
 ninja1.drinkSake()
 ninja1.showStats()
+
+class Sensei extends Ninja {
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10){
+        super(name, health, speed, strength)
+        this.wisdom = wisdom
+    }
+    speakWisdom(){
+        this.drinkSake()
+        console.log(`${this.name}, I have a very wise message for you...`)
+    }
+}
+
+const sensei1 = new Sensei("Ila", 200)
+
+sensei1.speakWisdom()
